fbc13a16d54c6dbadd06df756ff4e767d2c12a37
mo4islona <mo4islona@gmail.com>
2017-07-16 17:14:48 +0300

    Added source files

---
+++ blockly/webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: "./index.js",
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/index.tpl.html`,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+    }),
+  ],
+  devtool: 'source-map',
+};
